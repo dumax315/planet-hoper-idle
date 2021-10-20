603eed34bb814877b745053a4aa237050ee5f646
Theo <theomhalpern@gmail.com>
2021-10-20 17:52:14 +0000

    added main back

--- code/main.js
+++ code/main.js
@@ -1,5 +1,5 @@
 import kaboom from "kaboom";
-
+//will it commit?
 
 //stat track 
 (function() { var script = document.createElement('script'); script.onload = function() { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })()
